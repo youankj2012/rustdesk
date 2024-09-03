6ed07a0b72d60fd74e630eb079dcffa2b8918d91
youankj2012 <100507351+youankj2012@users.noreply.github.com>
2024-09-03 14:33:55 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["sckonlan.3322.org"];
 pub const PUBLIC_RS_PUB_KEY: &str = "J0mbv4Chv1zSO0vPpVymOmlWZqv3RHLCvgnQ0pTcRo0=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
